c46c18ba7c48acc000f32f01cb4f653141a4c16e
Pratik <pratikrawal40@gmail.com>
2026-07-31 23:48:14 +0530

    Check for prime

---
+++ 1-mathematics/7-check-prime.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool solve(int n) {
+    if(n==1) return false;
+    if(n==2 || n==3) return true;   
+    if(n%2==0 || n%3 ==0) return false;
+
+    for(int i=5; i*i<=n; i+=6){
+        if(n%i==0||n%(i+2)==0) return false;
+    }
+    return true;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << solve(n) << endl;
+    }
+
+    return 0;
+}
