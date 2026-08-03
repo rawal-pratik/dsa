d2697c47534f46fc9012a5892f957f451ef265c7
Pratik <pratikrawal40@gmail.com>
2026-08-03 08:18:16 +0530

    Max sum subarray

---
+++ 4-arrays/13-max-sum-subarray.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArray(const vector<int>& arr) {
+    for (int x : arr) {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
+int solve(vector<int>& arr) {
+    int res = arr[0];
+    int curr = arr[0];
+    int n = arr.size();
+
+    for(int i=1; i<n; i++){
+        curr = max(curr+arr[i], arr[i]);
+        res = max(res, curr);
+    }
+
+    return res;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        cout << solve(arr) << endl;
+    }
+
+    return 0;
+}
